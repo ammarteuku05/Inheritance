53b5229c37199f54d3bac8bb7ef68f95d7b16957
ammarteuku05 <57586371+ammarteuku05@users.noreply.github.com>
2020-03-04 12:10:15 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,9 @@
+package Tugas4;
+    public class BangunDatar {
+        public double Luas(){
+            return 0;
+        }
+       public double Keliling(){
+            return 0;
+        }
+}
